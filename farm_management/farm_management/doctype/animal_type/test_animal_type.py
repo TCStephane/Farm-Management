edc060088a719396450b5bd932856e292fd79e37
TCStephane <t.stephane@alustudent.com>
2025-12-02 15:34:57 +0000

    first push

---
+++ farm_management/farm_management/doctype/animal_type/test_animal_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Stephane and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAnimalType(FrappeTestCase):
+	pass
